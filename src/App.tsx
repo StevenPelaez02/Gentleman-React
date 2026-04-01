1cd8fd4b74e8a8d80fda25b35a340a556bf50769
StevenPelaez02 <estevenrengifo2002@hotmail.com>
2026-04-01 13:03:04 -0500

    UseEffect terminado, el anteriorr era use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { Button } from './components'
 
-class UseState {
-  value: any;
 
-  getValue() {
-    return this.value;
-  }
-
-  setValue(newValue: any) {
-    this.value = newValue;
-  }
-}
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('Alan')
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
   
-  const countMore = () => {
-  setCount((count) => count + 1)
-  setCount((count) => count + 1)
-  setCount((count) => count + 1)
-  setCount((count) => count + 1)
-  setCount((count) => count + 1)
-  setCount((count) => count + 1)
-  setCount((count) => count + 1)
-  setCount((count) => count + 1)
-  setCount((count) => count + 1)
+  const consoleLoader = (loadingValue: boolean) => {
+    setLoading(loadingValue)
+    console.info(loadingValue)
   }
 
-  const changeName = () => {
-  setName('Bob')
+  const fetchData = async () => {
+    
+    consoleLoader(true)
+
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+
+      if (!response.ok) {
+        throw new Error('Error al obtener los datos')
+      }
+
+      const jsonData = await response.json()
+      setData(jsonData)
+    } catch (err) {
+      setError(err as string)
+    } finally {
+      consoleLoader(false)
+    }
+  }
+
+  //comunicarnos con un endpoint -  ebtidad externa al componente
+  // operaciones asincronas
+  // parametros de entrada
+
+  // sync con entidades externas--
+  useEffect(() =>{
+    // 1 - cuando se monta el componente
+    // 2 - cada vez que se modifique un valor del state
+
+    fetchData()
+    
+    // manejar el estado de la memoria
+    // return () => { }
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  
+  if (error) {
+    return <div>Ups! Something went wrong.</div>
   }
 
   return (
-    <>
-      <Button label={`Count: ${count}`} parentMethod={countMore} />
-      <p>{ name }</p>
-      <Button label={`Count: ${count}`} parentMethod={changeName} />
-    </>
+    <div>{JSON.stringify(data)}</div>
   )
 }
 

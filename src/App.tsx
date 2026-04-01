e641405cc47ccf73a916088e163175fb375ebed5
StevenPelaez02 <estevenrengifo2002@hotmail.com>
2026-04-01 15:11:23 -0500

    custom Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useFetch } from './hooks'
 import './App.css'
 
+const url = 'https://pokeapi.co/api/v2/pokemon/ditto'
 
+interface Data{
+  name: string,
+  lastname: string,
+  age: number
+}
 
 function App() {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
   
-  const consoleLoader = (loadingValue: boolean) => {
-    setLoading(loadingValue)
-    console.info(loadingValue)
-  }
-
-  const fetchData = async () => {
-    
-    consoleLoader(true)
-
-    try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-
-      if (!response.ok) {
-        throw new Error('Error al obtener los datos')
-      }
-
-      const jsonData = await response.json()
-      setData(jsonData)
-    } catch (err) {
-      setError(err as string)
-    } finally {
-      consoleLoader(false)
-    }
-  }
-
-  //comunicarnos con un endpoint -  ebtidad externa al componente
-  // operaciones asincronas
-  // parametros de entrada
-
-  // sync con entidades externas--
-  useEffect(() =>{
-    // 1 - cuando se monta el componente
-    // 2 - cada vez que se modifique un valor del state
-
-    fetchData()
-    
-    // manejar el estado de la memoria
-    // return () => { }
-  }, [])
+  const { data, loading, error } = useFetch<Data>(url)
+  data
 
   if (loading) {
-    return <div>Loading...</div>
-  }
-  
-  if (error) {
-    return <div>Ups! Something went wrong.</div>
+    return <p>Loading...</p>
   }
 
-  return (
+  if (error) {
+    return <p>Error: {error.message}</p>
+  }
+  return(
     <div>{JSON.stringify(data)}</div>
   )
 }

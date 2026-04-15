a24de6d12416d65b38fe3291929ca2a041f69b7f
StevenPelaez02 <estevenrengifo2002@hotmail.com>
2026-04-15 14:51:44 -0500

    context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Button, ColorRed, AppForm } from './components'
-
+import { GlobalProvider } from './context/global.provider'
 
 function App() {
 
@@ -17,14 +17,15 @@ function App() {
   }
   
   return(
-    <>
+    <GlobalProvider>
       <ColorRed><Button parentMethod={dimeHola}>My boton rojo</Button></ColorRed>
       <Button parentMethod={handleClick}>My boton normal</Button>
       <AppForm>
-        <button type='submit' onClick={submit}></button>
+        <button type='submit' onClick={submit}>Submit</button>
       </AppForm>
-    </>
+    </GlobalProvider>
   )
 }
 
 export default App
+
